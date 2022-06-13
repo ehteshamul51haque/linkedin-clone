182c5b1d0e68b47036779c1a09e622aefeb47f75
Ehteshamul Haque <ehteshamulhaque5103@gmail.com>
2022-06-14 01:34:41 +0530

    Header.js&Header.css created, App.js modified

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="app">
       <h1>Linkedin Clone</h1>
+      {/* Header
+            App Body
+              SideBar
+              Feed
+              Widgets */}
     </div>
   );
 }
